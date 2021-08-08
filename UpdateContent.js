fff04a8099b5c8d69430a53ee526c6c011ff1949
Icegreenytea <44691277+Icegreenytea@users.noreply.github.com>
2021-08-08 17:17:49 +0900

    Add files via upload

--- UpdateContent.js
+++ UpdateContent.js
@@ -1,23 +1,49 @@
 import React, {Component} from 'react';
 
-class CreateContent extends Component{
+class UpdateContent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            id:this.props.data.id,
+            title:this.props.data.title,
+            des:this.props.data.des
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
     render(){
+        console.log(this.props.data);
         return(
             <article>
-                <h2>Create</h2>
-                <form action="/create_process" method="post"
+                <h2>Update</h2>
+                <form action="/update_process" method="post"
                     onSubmit={function(e){
                         e.preventDefault();
                         this.props.onSubmit(
-                            e.target.title.value,
-                            e.target.des.value
+                            this.state.id,
+                            this.state.title,
+                            this.state.des
                         );
                     }.bind(this)}
                 >
-                    <p><input type="text" name="title"
-                    placeholder="title"></input></p>
+                    <input type="hidden" name="id" value={this.state.id}></input>
                     <p>
-                        <textarea name="des" placeholder="description"></textarea>
+                        <input
+                        type="text"
+                        name="title"
+                        placeholder="title"
+                        value={this.state.title}
+                        onChange={this.inputFormHandler}
+                        ></input>
+                    </p>
+                    <p>
+                        <textarea
+                        onChange={this.inputFormHandler}
+                        name="des"
+                        placeholder="description"
+                        value={this.state.des}></textarea>
                     </p>
                     <p>
                         <input type="submit"></input>
@@ -28,4 +54,4 @@ class CreateContent extends Component{
   }
 }
 
-export default CreateContent;
+export default UpdateContent;
